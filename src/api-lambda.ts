5608e4e0003400d22cb5886299e38b1c30263335
tarmo <tarmokalling@gmail.com>
2023-06-08 17:15:23 +0300

    different way to invoke lambda handler

--- src/api-lambda.ts
+++ src/api-lambda.ts
@@ -3,10 +3,12 @@ import serverlessExpress from '@vendia/serverless-express';
 import { Callback, Context, Handler } from 'aws-lambda';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { initEnv } from './env';
 
 let server: Handler;
 
 async function bootstrap(): Promise<Handler> {
+  await initEnv();
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
