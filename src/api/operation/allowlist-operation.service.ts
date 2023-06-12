bc0ab1792e53f802daf45db15f44f47b40084850
Simo <info@analyticsflow.ee>
2023-06-12 19:16:51 +0300

    bugfix: operation hasrun number to boolean

--- src/api/operation/allowlist-operation.service.ts
+++ src/api/operation/allowlist-operation.service.ts
@@ -387,7 +387,7 @@ export class AllowlistOperationService {
       order: entity.op_order,
       allowlistId: entity.allowlist_id,
       createdAt: bigInt2Number(entity.created_at),
-      hasRan: entity.has_ran,
+      hasRan: !!entity.has_ran,
     };
   }
 }
