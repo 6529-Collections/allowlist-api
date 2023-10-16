fa7786942b89368cd0f55c88be0fd2834941e8ee
tarmo <tarmokalling@gmail.com>
2023-10-16 13:51:04 +0300

    More logging regarding Sentry

--- src/token-pool-downloader-lambda.ts
+++ src/token-pool-downloader-lambda.ts
@@ -10,7 +10,6 @@ import { TokenDownloaderModule } from './token-downloader.module';
 import { TokenPoolAsyncDownloader } from './token-pool/token-pool-async-downloader';
 import { TokenPoolDownloaderParams } from './token-pool/token-pool.types';
 import { withSentryIfConfigured } from './background-lambda-sentry';
-import * as Sentry from '@sentry/serverless';
 
 async function bootstrap(): Promise<INestApplication> {
   await initEnv();
@@ -25,64 +24,57 @@ export const handler = withSentryIfConfigured(
   async (event: any, context: Context) => {
     const nestApp = await bootstrap();
     const service = nestApp.get(TokenPoolDownloaderService);
-    try {
-      console.log('Received event', event);
-      const message = event.Records[0];
-      const params: TokenPoolDownloaderParams = JSON.parse(
-        JSON.parse(message.body).Message,
-      );
-      if (!params) {
-        throw new Error('No params provided');
-      }
-      const { config, state } = params;
-      if (!config || !state) {
-        throw new Error('No config or state provided');
-      }
-      const { tokenPoolId } = config;
-      if (!tokenPoolId) {
-        throw new Error('No id provided');
-      }
-      const { runsCount, startingBlocks } = state;
-      if (runsCount !== startingBlocks.length) {
-        console.error(
-          `Invalid state: runsCount ${runsCount} !== startingBlocks.length ${startingBlocks.length}`,
-        );
-        throw new Error('Invalid state');
-      }
-      state.runsCount++;
-      console.log('Run count', state.runsCount);
-      const result = await service.start({ config, state });
-      if (result?.continue) {
-        const downloadScheduler = nestApp.get(TokenPoolAsyncDownloader);
-        await downloadScheduler.start({
-          config: {
-            contract: result.entity.contract,
-            tokenIds: result.entity.token_ids,
-            tokenPoolId: result.entity.token_pool_id,
-            allowlistId: result.entity.allowlist_id,
-            blockNo: result.entity.block_no,
-            consolidateBlockNo: result.entity.consolidate_block_no,
-          },
-          state,
-        });
-      }
-      try {
-        await nestApp.get(DB).close();
-        await nestApp.close();
-      } catch (e) {
-        console.error(`Error closing server`, e);
-        throw e;
-      }
-      if (result?.error) {
-        throw new Error(result.error);
-      }
-      await context.succeed(event);
-    } catch (e) {
-      console.error('Error running worker. Capturing error', e);
-      await Sentry.captureException(e);
-      await Sentry.flush(3000);
-      await context.fail(e);
+    console.log('Received event', event);
+    const message = event.Records[0];
+    const params: TokenPoolDownloaderParams = JSON.parse(
+      JSON.parse(message.body).Message,
+    );
+    if (!params) {
+      throw new Error('No params provided');
     }
+    const { config, state } = params;
+    if (!config || !state) {
+      throw new Error('No config or state provided');
+    }
+    const { tokenPoolId } = config;
+    if (!tokenPoolId) {
+      throw new Error('No id provided');
+    }
+    const { runsCount, startingBlocks } = state;
+    if (runsCount !== startingBlocks.length) {
+      console.error(
+        `Invalid state: runsCount ${runsCount} !== startingBlocks.length ${startingBlocks.length}`,
+      );
+      throw new Error('Invalid state');
+    }
+    state.runsCount++;
+    console.log('Run count', state.runsCount);
+    const result = await service.start({ config, state });
+    if (result?.continue) {
+      const downloadScheduler = nestApp.get(TokenPoolAsyncDownloader);
+      await downloadScheduler.start({
+        config: {
+          contract: result.entity.contract,
+          tokenIds: result.entity.token_ids,
+          tokenPoolId: result.entity.token_pool_id,
+          allowlistId: result.entity.allowlist_id,
+          blockNo: result.entity.block_no,
+          consolidateBlockNo: result.entity.consolidate_block_no,
+        },
+        state,
+      });
+    }
+    try {
+      await nestApp.get(DB).close();
+      await nestApp.close();
+    } catch (e) {
+      console.error(`Error closing server`, e);
+      throw e;
+    }
+    if (result?.error) {
+      throw new Error(result.error);
+    }
+    await context.succeed(event);
     return {};
   },
 );
