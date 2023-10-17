6b786114508b23a69129427fac22f2bef5e356e6
tarmo <tarmokalling@gmail.com>
2023-10-17 17:11:06 +0300

    Removed Sentry debug mode and test lambda

--- src/test-app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SentryModule } from './sentry/sentry.module';
-
-@Module({
-  imports: [SentryModule.forRoot()],
-  controllers: [],
-})
-export class TestAppModule {}
