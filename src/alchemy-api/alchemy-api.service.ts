d61bdfd572535497a10854c84e380d973789342c
tarmo <tarmokalling@gmail.com>
2023-11-20 13:27:19 +0200

    Updated alchemy version
    Signed-off-by: tarmo <tarmokalling@gmail.com>

--- src/alchemy-api/alchemy-api.service.ts
+++ src/alchemy-api/alchemy-api.service.ts
@@ -1,13 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { Alchemy, NftContract } from 'alchemy-sdk';
+import { Alchemy } from 'alchemy-sdk';
 
 @Injectable()
 export class AlchemyApiService {
   constructor(private readonly alchemy: Alchemy) {}
-
-  async searchContractMetadata(kw: string): Promise<NftContract[]> {
-    return await this.alchemy.nft.searchContractMetadata(kw);
-  }
 
   async getBlockNumber(): Promise<number> {
     return await this.alchemy.core.getBlockNumber();
