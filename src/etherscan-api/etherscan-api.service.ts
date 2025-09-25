4ccbbb4233a5c4778a3a10890c813d6951a2a415
GelatoGenesis <tarmokalling@gmail.com>
2025-09-25 16:35:46 +0300

    etherscan integration fix
    Signed-off-by: GelatoGenesis <tarmokalling@gmail.com>

--- src/etherscan-api/etherscan-api.service.ts
+++ src/etherscan-api/etherscan-api.service.ts
@@ -9,22 +9,29 @@ export class EtherscanApiService {
   async currentBlockNumber(): Promise<number> {
     const blockNumber = await this.httpService.axiosRef
       .get(
-        'https://api.etherscan.io/api?module=proxy&action=eth_blockNumber&apikey=' +
+        'https://api.etherscan.io/v2/api?chainid=1&module=proxy&action=eth_blockNumber&apikey=' +
           process.env.ALLOWLIST_ETHERSCAN_API_KEY,
       )
       .then((res) => res.data.result);
-    return parseInt(blockNumber, 16);
+    const result = parseInt(blockNumber, 16);
+    if (isNaN(result)) {
+      throw new BadRequestException(
+        `Invalid response from Etherscan->currentBlockNumber: ${blockNumber}`,
+      );
+    }
+    return result;
   }
 
   async getBlockCountdown(param: {
     blockNumber: number;
   }): Promise<EtherscanGetBlockCountdownResponse> {
     const response = await this.httpService.axiosRef.get(
-      `https://api.etherscan.io/api?module=block&action=getblockcountdown&blockno=${param.blockNumber}&apikey=${process.env.ALLOWLIST_ETHERSCAN_API_KEY}}`,
+      `https://api.etherscan.io/v2/api?chainid=1&module=block&action=getblockcountdown&blockno=${param.blockNumber}&apikey=${process.env.ALLOWLIST_ETHERSCAN_API_KEY}}`,
     );
     if (response.data?.message !== 'OK' || response.data?.status !== '1') {
       throw new BadRequestException(
-        response.data.result ?? 'Something went wrong',
+        response.data.result ??
+          'Something went wrong with Etherscan->getBlockCountdown',
       );
     }
     return response.data;
