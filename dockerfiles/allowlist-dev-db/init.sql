de7581208620fdba1f9f0acb255327a6008f9ae6
GelatoGenesis <tarmokalling@gmail.com>
2026-05-19 12:52:36 +0300

    Better dev db setup
    Signed-off-by: GelatoGenesis <tarmokalling@gmail.com>

--- dockerfiles/allowlist-dev-db/init.sql
+++ dockerfiles/allowlist-dev-db/init.sql
@@ -1,4 +1,5 @@
-create user 'allowlist'@'%' identified with mysql_native_password by 'allowlist';
-create database allowlist;
-grant all privileges on allowlist.* TO 'allowlist';
-flush privileges;
+CREATE DATABASE IF NOT EXISTS allowlist;
+CREATE USER IF NOT EXISTS 'allowlist'@'%' IDENTIFIED WITH mysql_native_password BY 'allowlist';
+ALTER USER 'allowlist'@'%' IDENTIFIED WITH mysql_native_password BY 'allowlist';
+GRANT ALL PRIVILEGES ON allowlist.* TO 'allowlist'@'%';
+FLUSH PRIVILEGES;
